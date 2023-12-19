dd54fe80f9307af02a0872df4894990ca3ce7252
Cristian Statescu <135974225+CS9490@users.noreply.github.com>
2023-12-19 12:38:02 -0500

    Delete server directory

--- server/mci_modules/views.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-views = Blueprint('views', __name__)
-
-@views.route('/somepage')
-def some_page():
-    return "This is some page!"
--- server/test/test_user_long_string.py
+++
@@ -1,9 +0,0 @@
-def test_create_user_long_string(test_client):
-    long_string_data = {
-        "first_name": "a" * 1000,
-        "last_name": "Doe",
-        "email": "long@gmail.com",
-        "password": "password"
-    }
-    response = test_client.post('/register', json=long_string_data)
-    assert response.status_code == 201  # User is created successfully
